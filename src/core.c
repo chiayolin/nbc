6c0fc8bf0a56735298bfa838eaf1c1024fe7ff93
chiayolin <chiayo.lin@gmail.com>
2014-11-09 08:12:36 +0700

    Rewritten functions into header

--- src/core.c
+++ src/core.c
@@ -1,5 +1,5 @@
 /*
- * dtob - dtob.c
+ * dtob - core.c
  * Binary to Decimal and Decimal to Binary Converter
  * Copyright (C) 2014  Chiayo Lin <chiayo.lin@gmail.com>
  *
@@ -16,89 +16,16 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "core.h"
+ 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <math.h>
 
-#define MAX 1000
-
-int getaton(int array[MAX]); /* Read ASCII numbers into an array, convert to numbers, return size */
-int btod(int binary[MAX], const int size); /* Convert Bin. to Dec. */
-int dtob(int decimal, int binary[MAX]); /* Convert Dec. to Bin. */
-void printa(const int array[MAX], const int size); /* Print an array */
-void reverse(int array[MAX], const int size); /* Reverse an array */
-void interactive(); /* Interactive mode */
-void help(int type); /* Print help messages base on the varible 'type' */
-void info(); /* Print some information about this program */
-
-int main(const int argc, char *argv[]) {
-	int binary[MAX], decimal;
-	int size;
-	
-	/* Enter interactive mode if there are no command line options */
-	if(argc == 1) {
-		interactive();
-	} 
-	
-	/* Read the options if argv[0][0] = '-' */
-	else if((*++argv)[0] == '-') {
-		int binary[MAX], decimal, size;
-		char opt = *++argv[0];
-		switch(opt) {
-			case 'd':
-				if(argv[1] == NULL)
-					printf("agument to '-%s' is missing, expected a decimal number\n", argv[0]);
-				else {
-					decimal = atoi(argv[1]);
-					size = dtob(decimal, binary);
-					printa(binary, size);
-				}
-				break;
-
-			case 'b':
-				if(argv[1] == NULL)
-					printf("agument to '-%s' is missing, expected a binary number\n", argv[0]);
-				else {
-					size = strlen(argv[1]);
-					int i;
-					for(i = 0; i < size; i++)
-						binary[i] = argv[1][i] - '0'; 
-					decimal = btod(binary, size);
-					printf("%d\n", decimal);
-				}
-				break;
-
-			case 'h':
-				help(1);
-				break;
-
-			case 'i':
-				interactive();
-				break;
-
-			case 'm':
-				info();
-				break;
-
-			default:
-				 printf("dtob: alas, invalid option '-%s' \n", argv[0]);
-				 help(0);
-				 break;
-		}
-	}
-	
-	/* Else, print the error message */
-	else {
-		printf("dtob: alas, syntax error :-(\n");
-		help(0);
-		return 1;
-	}
-			
-	return 0;
-}	
-
+/* 
+ * Convert Dec. to Bin.
+ */
 int dtob(int decimal, int binary[MAX]) {
 	int i, size;
 	if(decimal == 0) {
@@ -115,6 +42,9 @@ int dtob(int decimal, int binary[MAX]) {
 	return size; /* Return the size of array */
 }
 
+/* 
+ * Convert Bin. to Dec. 
+ */
 int btod(int binary[MAX], const int size) {
 	int i, decimal;
 	i = decimal = 0;
@@ -126,6 +56,9 @@ int btod(int binary[MAX], const int size) {
 	return decimal;
 }
 
+/* 
+ * Read ASCII numbers into an array, convert to numbers, return array size 
+ */
 int getaton(int array[MAX]) {
 	int i, c, size;
 	getchar(); /* To avoid a return '\n' */
@@ -137,6 +70,9 @@ int getaton(int array[MAX]) {
 	return size;
 }
 
+/*
+ * Print an array
+ */
 void printa(const int array[MAX], const int size) {
 	int i;
 	for(i = 0; i < size; i++)
@@ -144,6 +80,9 @@ void printa(const int array[MAX], const int size) {
 	printf("\n");
 }
 
+/*
+ * Reverse an array
+ */
 void reverse(int array[MAX], const int size) {
 	int temp[MAX], i;
 	for(i = 0; i < size; i++)
@@ -153,6 +92,9 @@ void reverse(int array[MAX], const int size) {
 		array[i] = temp[j];
 }
 
+/*
+ * Interactive mode
+ */
 void interactive() {
 	int binary[MAX], decimal, size;
 
@@ -196,6 +138,9 @@ void interactive() {
 	}
 }
 
+/* 
+ * Print help messages base on the varible 'type'
+ */
 void help(int type) {
 	const char h[] = "print this usage and text\n",
 	    	   d[] = "convert decimal to binary\n",
@@ -227,6 +172,9 @@ void help(int type) {
 	}
 }
 
+/*
+ * Print some information about this program
+ */
 void info() {
 	printf("dtob (Version 1.0) Copyrights (C) 2014 Chiayo Lin\n");
 	printf("Binary to Decimal and Decimal to Binary Converter\n\n");
---
+++ src/main.c
@@ -0,0 +1,90 @@
+/*
+ * dtob - main.c
+ * Binary to Decimal and Decimal to Binary Converter
+ * Copyright (C) 2014  Chiayo Lin <chiayo.lin@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include "core.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(const int argc, char *argv[]) {
+	int binary[MAX], decimal;
+	int size;
+	
+	/* Enter interactive mode if there are no command line options */
+	if(argc == 1) {
+		interactive();
+	} 
+	
+	/* Read the options if argv[0][0] = '-' */
+	else if((*++argv)[0] == '-') {
+		int binary[MAX], decimal, size;
+		char opt = *++argv[0];
+		switch(opt) {
+			case 'd':
+				if(argv[1] == NULL)
+					printf("agument to '-%s' is missing, expected a decimal number\n", argv[0]);
+				else {
+					decimal = atoi(argv[1]);
+					size = dtob(decimal, binary);
+					printa(binary, size);
+				}
+				break;
+
+			case 'b':
+				if(argv[1] == NULL)
+					printf("agument to '-%s' is missing, expected a binary number\n", argv[0]);
+				else {
+					size = strlen(argv[1]);
+					int i;
+					for(i = 0; i < size; i++)
+						binary[i] = argv[1][i] - '0'; 
+					decimal = btod(binary, size);
+					printf("%d\n", decimal);
+				}
+				break;
+
+			case 'h':
+				help(1);
+				break;
+
+			case 'i':
+				interactive();
+				break;
+
+			case 'm':
+				info();
+				break;
+
+			default:
+				 printf("dtob: alas, invalid option '-%s' \n", argv[0]);
+				 help(0);
+				 break;
+		}
+	}
+	
+	/* Else, print the error message */
+	else {
+		printf("dtob: alas, syntax error :-(\n");
+		help(0);
+		return 1;
+	}
+			
+	return 0;
+}	
