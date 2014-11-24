7e7d1cbc37a9f88cb1ce37ccdd4c8db3975835bf
chiayolin <chiayo.lin@gmail.com>
2014-11-24 15:50:32 +0700

    Added color code

--- src/core.h
+++ src/core.h
@@ -22,6 +22,17 @@
 #define VERSION "2.0(x)-nbc_develop" /* Program version */
 #define MAX 1000 		     /* Maximum arrray size */
 
+/* Define colors */
+#define KNRM  "\x1B[0m"	 /* Normal */
+#define KRED  "\x1B[31m" /* Red */
+#define KGRN  "\x1B[32m" /* Green*/
+#define KYEL  "\x1B[33m" /* Yellow */
+#define KBLU  "\x1B[34m" /* Blue */
+#define KMAG  "\x1B[35m" /* Magenta */
+#define KCYN  "\x1B[36m" /* Cyan */
+#define KWHT  "\x1B[37m" /* White */
+#define RESET "\033[0m"  /* Reset */
+
 enum { 
 	SET = 0, HELP, INFO, QUIT, STATE, CONVERT, 
 	INPUT, OUTPUT, BIN, OCT, DEC, HEX }; /* Numberic value for tokens */
@@ -31,6 +42,7 @@ int btod(char *binary, const int size); 	/* Convert Bin. to Dec. */
 int otod(char *octal, const int size);		/* Convert Oct. to Dec. */
 int dtob(int decimal, char *binary);	 	/* Convert Dec. to Bin. */
 int dtoo(int decimal, char *octal);		/* Convert Dec. to Oct. */
+int dtoh(int decimal, char *hexadecimal); 	/* Convert Dec. to Hex. */
 
 void printa(const char *array, const int size); /* Print an array */
 void reverse(char *array, const int size); /* Reverse an array */
